cadd851142034f076d5ee21e0c22251fead5bd91
aopszala <92755481+olohiperbolo@users.noreply.github.com>
2024-02-23 22:28:27 +0100

    pliki

---
+++ zad4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+//zad4
+int main() {
+    for(int i = 1; i <= 200; i++){
+        printf("\n%d",i);
+        if(i % 7 == 0 && i % 5 == 0){
+            printf("FooBar");
+        }
+        else if(i % 5 == 0){
+            printf("Foo");
+        }
+        else if(i % 7 == 0){
+            printf("Bar");
+        }
+
+    }
+}
